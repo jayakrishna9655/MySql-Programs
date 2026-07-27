54abb5645be523aed39175d12e4eae6a8b2cd164
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2026-07-27 23:00:47 +0530

    Add files via upload

--- joining-multi-table.sql
+++ joining-multi-table.sql
@@ -3,3 +3,8 @@ USE mysqllearning;
 SELECT o.order_no, c.name,c.address,sh.name AS "Shop name",sh.dept AS "Shop Dept" FROM customers c
       JOIN orders o on o.customer_id = c.customer_id 
       JOIN mydb.shop sh WHERE sh.name = c.name;
+      
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+SELECT * FROM customers c JOIN orders o ON o.customer_id = c.customer_id AND  o.order_id = c.customer_id;
